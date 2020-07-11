d294eb78dd2c3705ab90eef4ff3b63e46bfa0646
Jonathan <jondeleon856@gmail.com>
2020-07-11 17:56:50 -0400

    Hook up the styles, footer,header, and js files to the functions.php

---
+++ app/public/wp-content/themes/jonathanTheme/header.php
@@ -0,0 +1,5 @@
+<html lang="en">
+<head>
+    <?php wp_head(); ?>
+</head>
+<body>
--- app/public/wp-content/themes/jonathanTheme/index.php
+++ app/public/wp-content/themes/jonathanTheme/index.php
@@ -3,4 +3,9 @@
 Template Name: Custom Homepage
 */ ?>
 
+<?php get_header(); ?>
+
 <h1>Hello</h1>
+
+
+<?php get_footer(); ?>
