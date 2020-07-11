1672dbad97d12e3748a69fda66f7bdf727bdf485
Jonathan <jondeleon856@gmail.com>
2020-07-11 19:39:11 -0400

    Desktop Top Section

--- app/public/wp-content/themes/jonathanTheme/header.php
+++ app/public/wp-content/themes/jonathanTheme/header.php
@@ -6,12 +6,18 @@
 <header>
     <div class="inner-container">
         <div class="logo-container">
-            <img class="logo-img" src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" alt="">
+            <!-- <img class="logo-img" src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" alt=""> -->
+            <div class="name-container">
+                <div class="text-container">
+                    <h1 class="name">Alejandro Munoz</h1>
+                    <p class="titles">Director | Producer | Creator</p>
+                </div>
+            </div>
         </div>
         <nav class="nav-container">
-            <a class="nav-link" href="#">Music Video</a>
-            <a class="nav-link" href="#">Narrative</a>
-            <a class="nav-link" href="#">Commercial</a>
+            <a class="nav-link" href="#">Services</a>
+            <a class="nav-link" href="/about-me">About Me</a>
+            <a class="nav-link" href="#">Contact</a>
         </nav>
     </div>
 </header>
--- app/public/wp-content/themes/jonathanTheme/index.php
+++ app/public/wp-content/themes/jonathanTheme/index.php
@@ -5,6 +5,14 @@ Template Name: Custom Homepage
 
 <?php get_header(); ?>
 
+<section class="sec-001">
+    <div class="inner-section">
+        <div class="video-container">
+        <iframe class="main-video" src="https://www.youtube.com/embed/PrP-HvexG24" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
+</section>
+
 <h1>Hello</h1>
 
 
