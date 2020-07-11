ed23d3881bc4d8a994a0b74aa73329d9813dc004
Jonathan <jondeleon856@gmail.com>
2020-07-11 18:48:53 -0400

    Build header for website

--- app/public/wp-content/themes/jonathanTheme/header.php
+++ app/public/wp-content/themes/jonathanTheme/header.php
@@ -3,3 +3,15 @@
     <?php wp_head(); ?>
 </head>
 <body>
+<header>
+    <div class="inner-container">
+        <div class="logo-container">
+            <img class="logo-img" src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" alt="">
+        </div>
+        <nav class="nav-container">
+            <a class="nav-link" href="#">Music Video</a>
+            <a class="nav-link" href="#">Narrative</a>
+            <a class="nav-link" href="#">Commercial</a>
+        </nav>
+    </div>
+</header>
