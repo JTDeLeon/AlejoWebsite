42d6999f73eeb6753bffabd4f53431a0e71d75fc
Jonathan <jondeleon856@gmail.com>
2020-07-11 20:18:52 -0400

    Added section 002 desktop

--- app/public/wp-content/themes/jonathanTheme/index.php
+++ app/public/wp-content/themes/jonathanTheme/index.php
@@ -13,6 +13,54 @@ Template Name: Custom Homepage
     </div>
 </section>
 
+<section class="sec-002">
+    <div class="inner-section">
+        <div class="top-sec">
+            <div class="text-container">
+                <h2>LEARN WHAT YOU NEED<br/><span class="color-accent">TO CREATE WHAT YOU WANT</span></h2>
+                <p>I help creators and artists increase their public image through providing high-quality productions, social media content, and a successful mindset to help them reach their dreams</p>
+            </div>
+            <div class="btn-container">
+                <a class="btn" href="#">Book Now</a>
+            </div>
+        </div>
+
+        <div class="services-sec">
+            <div class="column">
+                <div class="imagery">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/OuQ0iSNGwgU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <p class="imagetitle">Music Video Title</p>
+                <p class="description">We help both national and local musicians create captivating music videos, providing professional video production services and creative insight. We deliver the best visual stimuli in all the land. let us help you grow your brand and promote your music.</p>
+                <div class="btn-container">
+                    <a class="btn" href="#">Learn More</a>
+                </div>
+            </div>
+            <div class="column">
+                <div class="imagery">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/OuQ0iSNGwgU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <p class="imagetitle">Music Video Title</p>
+                <p class="description">We help both national and local musicians create captivating music videos, providing professional video production services and creative insight. We deliver the best visual stimuli in all the land. let us help you grow your brand and promote your music.</p>
+                <div class="btn-container">
+                    <a class="btn" href="#">Learn More</a>
+                </div>
+            </div>
+            <div class="column">
+                <div class="imagery">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/OuQ0iSNGwgU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <p class="imagetitle">Music Video Title</p>
+                <p class="description">We help both national and local musicians create captivating music videos, providing professional video production services and creative insight. We deliver the best visual stimuli in all the land. let us help you grow your brand and promote your music.</p>
+                <div class="btn-container">
+                    <a class="btn" href="#">Learn More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
 <h1>Hello</h1>
 
 
